29bd05880dffe318e5f3e4c052934b0cb699194f
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-07-03 14:17:39 +0100

    feat: setting up basic route

--- public/index.php
+++ public/index.php
@@ -2,4 +2,18 @@
 
 require "../helper.php";
 
-loadView("home");
+
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php',
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if (array_key_exists($uri, $routes)) {
+    require(getBasePath($routes[$uri]));
+} else {
+    require(getBasePath($routes['404']));
+}
