912e9bc7337ca544e7fe6cd20dbd20b1f7e41546
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-07-17 15:27:58 +0100

    feat: update listing

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -8,12 +8,14 @@ use Framework\Validation;
 class ListingController
 {
     protected $db;
+    protected $allowedFields;
 
     public function __construct()
     {
         $config = require getBasePath('config/db.php');
 
         $this->db = new Database($config);
+        $this->allowedFields = ['title', 'description', 'salary', 'requirements', 'benefits', 'company', 'address', 'city', 'state', 'phone', 'email', 'tags'];
     }
 
     /**
@@ -66,9 +68,7 @@ class ListingController
 
     public function store()
     {
-        $allowedFields = ['title', 'description', 'salary', 'requirements', 'benefits', 'company', 'address', 'city', 'state', 'phone', 'email', 'tags'];
-
-        $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
+        $newListingData = array_intersect_key($_POST, array_flip($this->allowedFields));
 
         $newListingData['user_id'] = 1;
 
@@ -178,5 +178,72 @@ class ListingController
         loadView('listings/edit', [
             'listing' => $listing
         ]);
+    }
+
+
+    /**
+     * Updates a listing in the database.
+     * 
+     * @param array $params
+     * @return void
+     */
+    public function update($params)
+    {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing =  $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        $updateValues = array_intersect_key($_POST, array_flip($this->allowedFields));
+
+        $updateValues = array_map('sanitize', $updateValues);
+
+        $requiredFields = ['title', 'description', 'email', 'salary', 'city', 'state'];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (empty($updateValues[$field]) || !Validation::string($updateValues[$field])) {
+                $errors[$field] = ucfirst($field) . " is required";
+            }
+
+            if ($field === 'email' && !Validation::email($updateValues[$field])) {
+                $errors[$field] = "Invalid email address";
+            }
+        }
+
+        if (!empty($errors)) {
+            loadView('listings/edit', [
+                'errors' => $errors,
+                'listing' => $listing,
+            ]);
+            exit;
+        } else {
+            // Submit the form
+            $updateFields = [];
+            foreach (array_keys($updateValues) as $field) {
+                $updateFields[] = "{$field} = :{$field}";
+            }
+
+            $updateFields = implode(', ', $updateFields);
+
+            $updateQuery = "UPDATE listings SET {$updateFields} WHERE id = :id";
+
+            $updateValues['id'] = $id;
+            $this->db->query($updateQuery, $updateValues);
+
+            //Set flash message
+            $_SESSION['success_message'] = 'Listing updated successfully';
+
+            redirect('/listings/' . $id);
+        }
     }
 }
